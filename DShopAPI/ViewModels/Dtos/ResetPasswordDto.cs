e1d8947d2b357ff0df8cd44de0a3fb85c8056b64
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-06-29 11:58:44 +0100

    new

--- DShopAPI/ViewModels/Dtos/ResetPasswordDto.cs
+++ DShopAPI/ViewModels/Dtos/ResetPasswordDto.cs
@@ -2,6 +2,14 @@
 
 namespace DShopAPI.ViewModels.Dtos
 {
+    public class ResetPasswordDto
+    {
+        public string ResetToken { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+
     public class ResetPasswordInitiateDto
     {
         [Required]
